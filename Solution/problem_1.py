ac83ad584654c3ec744807738781c1ade9c14540
root <root@Kamrul.localdomain>
2019-03-28 00:42:03 +0600

    Adding 2nd problem solution and documentation of 1st problem

--- Solution/problem_1.py
+++ Solution/problem_1.py
@@ -1,13 +1,17 @@
 """
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5,   
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 
 def main():
+# Limit//n -> tells how many multiples there are in limit of 'n'
+# substrating summation of the multiples of 15 beacause this part is 
+# added both by summation of 3 and 5
   LIMITATION = 1000
-  LIMIT = LIMITATION-1
+  LIMIT = LIMITATION-1    # Under Limitation thus -1
   sum_of_3 = summation((LIMIT//3))*3
   sum_of_5 = summation((LIMIT//5))*5
   sum_of_15 = summation((LIMIT//15))*15
